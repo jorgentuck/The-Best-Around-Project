8776464551aa3b4a74142429e66bec5a49fad1c2
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-01-04 19:59:04 -0700

    creating a sign up place on log in page.

--- controllers/routes/homeRoutes.js
+++ controllers/routes/homeRoutes.js
@@ -10,7 +10,6 @@ const {
   Videos,
   Votes,
 } = require('../../models');
-
 router.get('/', async (req, res) => {
   try {
     res.render('homepage');
@@ -18,20 +17,17 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 router.get('/login', async (req, res) => {
   try {
     if (req.session.logged_in) {
       res.redirect('/profile');
       return;
     }
-
     res.render('login');
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 // router.get('/profile', async (req, res) => {
 router.get('/profile', checkAuth, async (req, res) => {
   try {
@@ -40,7 +36,6 @@ router.get('/profile', checkAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 // router.get('/upload', async (req, res) => {
 router.get('/upload', checkAuth, async (req, res) => {
   try {
@@ -49,49 +44,69 @@ router.get('/upload', checkAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+router.get('/sign-s3', checkAuth, async (req, res) => {
+  const userId = req.session.user_id;
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: userId.concat(fileName),
+    Expires: 120,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+router.post('/save-details', (req, res) => {
+  // TODO: Read POSTed form data and do something useful
+});
 router.post('/login', async (req, res) => {
   try {
     const user = await Users.findOne({
       where: { email_address: req.body.email_address },
     });
-
     if (!user) {
-      res.status(404).json({ message: 'Login failed! email not found' });
+      res.status(404).json({ message: 'Login failed!' });
       return;
     }
-
     const passValidation = await bcrypt.compare(
       req.body.password,
       user.password
     );
-
     if (!passValidation) {
-      res.status(404).json({ message: 'Login failed! password wrong' });
+      res.status(404).json({ message: 'Login failed!' });
       return;
     }
     req.session.save(() => {
       req.session.user_id = user.id;
       req.session.logged_in = true;
-
       // res.status(200).json({ message: 'Login Success!' });
       // res.status(200).json(req.session);
-      res.render('profile');
+      res.redirect('/profile');
     });
-
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-router.post('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.render('homepage');
+      res.redirect('/');
     });
   } else {
     res.status(404).end();
   }
 });
-
 module.exports = router;
