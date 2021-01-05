e7b40dc483dcd802cdec829bf141402a942a51d9
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-01-04 19:10:32 -0700

    fixing main and homepage

---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+  
+   
+    const email = document.querySelector('#email_address').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (email && password) {
+      
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  
